ebe315c1e2b3c85f06efb220d62be3ff6cf6527a
Duncan van Tongeren <dpvantongeren@gmail.com>
2020-01-28 19:03:55 +0700

    Adding a new module PLayer.

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,11 +1,11 @@
 defmodule TextClient.Interact do
 
-    alias TextClient.State
+    alias TextClient.{ Player, State }
 
     def start() do
         Hangman.new_game()
         |> setup_state()  
-        |> IO.inspect()  
+        |> Player.play()
     end
 
     def play(state) do
---
+++ text_client/lib/text_client/player.ex
@@ -0,0 +1,62 @@
+defmodule TextClient.Player do
+    
+    alias TextClient.State
+
+    # won, lost, good_guess, bad_guess, already_used, initialising
+    def play(%State{tally: %{ game_state: :won }}) do
+        exit_with_message("You WON!") 
+    end
+
+    def play(%State{tally: %{ game_state: :lost }}) do
+        exit_with_message("Sorry, you lost.") 
+    end
+
+    def play(game = %State{tally: %{ game_state: :good_guess }}) do
+        continue_with_message(game, "Good guess!")
+    end
+
+    def play(game = %State{tally: %{ game_state: :bad_guess }}) do
+        continue_with_message(game, "Sorry, that isn't in the word.")
+    end
+
+    def play(game = %State{tally: %{ game_state: :already_used }}) do 
+        continue_with_message(game, "You've already used that letter.")
+    end
+
+    def play(game) do
+        continue(game)
+    end
+
+    def display(game) do
+        game
+    end
+
+    def prompt(game) do
+        game
+    end
+
+    def make_move(game) do
+        game
+    end
+
+    def continue(game) do
+        game
+        |> display()
+        |> prompt()
+        |> make_move()
+        |> play()
+    end
+
+    # PRIVATE ------------------------------------------------------
+
+    defp exit_with_message(msg) do
+        IO.puts(msg)
+        exit(:normal)
+    end
+
+    defp continue_with_message(game, msg) do
+        IO.puts(msg)
+        continue(game)
+    end
+
+end
